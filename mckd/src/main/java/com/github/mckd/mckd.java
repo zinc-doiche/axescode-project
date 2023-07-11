652a1b74a53aa93763390645285d151e81024227
mckd1717 <bug.rightnetwork@gmail.com>
2023-07-11 18:07:27 +0900

    모듈 추가

--- mckd/src/main/java/com/github/mckd/mckd.java
+++ mckd/src/main/java/com/github/mckd/mckd.java
@@ -2,7 +2,7 @@ package com.github.mckd;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Mckd extends JavaPlugin {
+public final class mckd extends JavaPlugin {
 
     @Override
     public void onEnable() {
